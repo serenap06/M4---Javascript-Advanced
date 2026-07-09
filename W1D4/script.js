33d9082e951598f8e0853751cdd8bc527206c190
serenap06 <168187800+serenap06@users.noreply.github.com>
2026-07-09 18:21:14 +0200

    aggiunta funzione al click

--- W1D4/script.js
+++ W1D4/script.js
@@ -2,9 +2,19 @@
 // Creare una funzione che cicla i risultati ricevendo come parametro l'array delle canzoni 
 // Creare una funzione che data una canzone come parametro crea la card nell'HTML 
 const searchBox = document.getElementById('searchResults')
+const searchBtn = document.getElementById('button-search')
+const searchInput = document.getElementById('searchField')
 
-function getSongs() {
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem')
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const searchValue = searchInput.value
+    getSongs(searchValue)
+})
+
+function getSongs(search = 'eminem') {
+    const urlApi = `https://striveschool-api.herokuapp.com/api/deezer/search?q=${search}`
+
+    fetch(urlApi)
         .then(result => result.json())
         .then(songs => listSongs(songs.data))
         .catch(error => console.log(error))
@@ -12,12 +22,13 @@ function getSongs() {
 getSongs()
 
 function listSongs(songs) {
-    for (const song of songs) {
+    searchBox.innerHTML = ""
+    songs.forEach(song => {
         const card = cardSongs(song)
-        console.log(card)
         searchBox.appendChild(card)
-    }
+    });
 }
+
 
 function cardSongs(song) {
     // <div class="card" style="width: 18rem;">
@@ -30,19 +41,19 @@ function cardSongs(song) {
     // </div>
 
     const col = document.createElement('div')
-    col.classList.add('col-4')
+    col.classList.add('col-4', 'my-3')
 
     const card = document.createElement('div')
-    card.classList.add('card')
+    card.classList.add('card', 'h-100')
     col.appendChild(card)
 
     const img = document.createElement('img')
-    img.classList.add('card-img-top')
+    img.classList.add('card-img-top', 'img-fluid', 'w-100')
     img.src = song.album.cover_small
     card.appendChild(img)
 
     const cardBody = document.createElement('div')
-    cardBody.classList.add('card-body')
+    cardBody.classList.add('card-body', 'd-flex', 'flex-column', 'justify-content-between')
     card.appendChild(cardBody)
 
     const cardTitle = document.createElement('h5')
@@ -51,12 +62,12 @@ function cardSongs(song) {
     cardBody.appendChild(cardTitle)
 
     const cardText = document.createElement('p')
-    cardText.classList.add('card-text', 'text-dark')
+    cardText.classList.add('card-text', 'text-dark', 'fs-5')
     cardText.innerHTML = song.artist.name
     cardBody.appendChild(cardText)
 
     const cardBtn = document.createElement('a')
-    cardBtn.classList.add('btn', 'btn-dark')
+    cardBtn.classList.add('btn', 'btn-block', 'btn-success')
     cardBtn.innerText = 'Play'
     cardBtn.href = song.link
     cardBody.appendChild(cardBtn)
